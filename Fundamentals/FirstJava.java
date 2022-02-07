dbcc5df3cc5a0c7e11665c2acc5b5af91bd82a05
Craig Brinkman <craigfrancis77@gmail.com>
2022-02-07 13:38:13 -0700

    First Java

---
+++ Fundamentals/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+	public static void main(String[] args) {
+		System.out.println("My Name is Craig");
+		System.out.println("I am 28 years old");
+		System.out.println("My hometown is Fort Collins");
+	}
+}
