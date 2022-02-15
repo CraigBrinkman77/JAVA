81d26372be94bd01a16c21a7078e81be8715d2be
Craig Brinkman <craigfrancis77@gmail.com>
2022-02-14 17:16:14 -0700

    FRUITZ

---
+++ Spring/fruityLoop/src/test/java/com/craig/fuityLoops/FruityLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craig.fuityLoops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
